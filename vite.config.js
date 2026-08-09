92a35680b18a1196ee7e32e90027df364568dd6a
lionel-alvarado16 <lalvaradoanastacio@byupathway.edu>
2026-08-09 00:04:43 -0500

    Final Project Progress

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src/",
+  root: ".",
+  publicDir: "src/public",
 
   build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
+        main: resolve(__dirname, "index.html"),
       },
     },
   },
